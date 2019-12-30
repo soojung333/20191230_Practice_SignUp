d824fce30cfb49082f9b458848de368e9acb5ed5
soojung <blessme333@gmail.com>
2019-12-30 15:15:20 +0900

    회원가입 화면 초안

--- app/src/main/java/co/soo/a20191230_practice_signup/SignUpActivity.kt
+++ app/src/main/java/co/soo/a20191230_practice_signup/SignUpActivity.kt
@@ -1,26 +1,32 @@
 package co.soo.a20191230_practice_signup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : BaseActivity() {
+//    SignUpActivity로 변경함!
+class SignUpActivity : BaseActivity() {
+
+    var lastBackTimeValue = 0L
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
 
         setupEvents()
         setValues()
-        
+
     }
 
 
     override fun setupEvents() {
 
+        lastBackTimeValue = 10L
+
     }
 
     override fun setValues() {
+
+        lastBackTimeValue = 20L
 
     }
 
