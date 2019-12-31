76d49e13ef1cdae87cdc3fc37996569457650ee0
soojung <blessme333@gmail.com>
2019-12-31 11:17:57 +0900

    망해가는부분

--- app/src/main/java/co/soo/a20191230_practice_signup/SignUpActivity.kt
+++ app/src/main/java/co/soo/a20191230_practice_signup/SignUpActivity.kt
@@ -1,11 +1,13 @@
 package co.soo.a20191230_practice_signup
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 //    SignUpActivity로 변경함!
 class SignUpActivity : BaseActivity() {
 
-    var lastBackTimeValue = 0L
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +22,37 @@ class SignUpActivity : BaseActivity() {
 
     override fun setupEvents() {
 
-        lastBackTimeValue = 10L
+//        pwEdt.addTextChangedListener(object : TextWatcher {
+//            override fun afterTextChanged(s: Editable?) {
+//
+//            }
+//
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//            }
+//
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//            }
+//
+//        })
+
+
+        pwEdt. ??? {
+            val inputStr = it.toString()
+            if (inputStr.length == 0 ) {
+                pwStatusTxt.text = "비밀번호가 입력되지 않았습니다"
+            }
+            else if (inputStr.length < 8) {
+                pwStatusTxt.text = "입력된 비번이 너무 짧습니다"
+            }
+            else {
+                pwStatusTxt.text = "사용해도 좋은 비밀번호입니다"
+            }
+        }
 
     }
 
     override fun setValues() {
 
-        lastBackTimeValue = 20L
 
     }
 
